07974858094d761660c2990ec5d606623b924baa
Ashwin <mahesh.ashwin1998@gmail.com>
2018-09-19 17:00:09 -0400

    Angular setup

---
+++ public/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpService } from './http.service'
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule, HttpClient } from '@angular/common/http'
+import { FormsModule } from '@angular/forms'
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [HttpService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ server.js
@@ -0,0 +1,9 @@
+var express=require('express')
+var app=express()
+var path=require('path')
+
+app.use(express.static(path.join(__dirname, './public/dist/public')))
+
+app.listen(8000, function(){
+    console.log("Server is listening on port 8000")
+})
