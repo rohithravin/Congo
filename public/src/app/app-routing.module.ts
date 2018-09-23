8b2c7a4867fc0851c201ec07c831d23b414aafb7
Spencer Kline <kline39@purdue.edu>
2018-09-23 19:10:58 -0400

    Setup Product page

--- public/src/app/app-routing.module.ts
+++ public/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { SearchComponent } from './search/search.component';
+import { ProductComponent } from './product/product.component';
 
 const routes: Routes = [
-  {path: 'search', component: SearchComponent}
+  {path: 'search', component: SearchComponent},
+  {path: 'product', component: ProductComponent}
 ];
 
 
