d6be2c14d3a15743867965cfe79b94ad8b17b908
Rohith Ravindranath <rohithravin@gmail.com>
2018-09-25 19:58:51 -0400

    compeleted pages
    Cart Page
    Checkout Page
    Checkout- Conf Page
    All completed

--- public/src/app/app-routing.module.ts
+++ public/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { CartComponent } from './cart/cart.component'
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
 import { CheckoutConfComponent } from './checkout-conf/checkout-conf.component';
 
 const routes: Routes = [
-  {path:'cart', component: CartComponent}
+  {path:'cart', component: CartComponent},
+  {path:'checkout', component: CheckoutComponent },
   {path:'checkout-conf', component: CheckoutConfComponent }
 ];
 
