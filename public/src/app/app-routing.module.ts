de400cce61b31b3729243feb9bf4b905eb7c66a1
Rohith Ravindranath <rohithravin@gmail.com>
2018-09-25 17:36:56 -0400

    my cart and confirmation page done

--- public/src/app/app-routing.module.ts
+++ public/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CartComponent } from './cart/cart.component'
+import { CheckoutConfComponent } from './checkout-conf/checkout-conf.component';
 
 const routes: Routes = [
   {path:'cart', component: CartComponent}
+  {path:'checkout-conf', component: CheckoutConfComponent }
 ];
 
 @NgModule({
