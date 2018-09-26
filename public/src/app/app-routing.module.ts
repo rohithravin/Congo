c9992fb5ba5e62c2d2a3d1867be4fabc3f6db8ac
Rohith Ravindranath <rohithravin@gmail.com>
2018-09-26 10:44:03 -0400

    completed login pages
    User Login Page
    Merchant Login Page
    Two new components for login pages

--- public/src/app/app-routing.module.ts
+++ public/src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { CartComponent } from './cart/cart.component';
 import { CheckoutComponent } from './checkout/checkout.component';
 import { CheckoutConfComponent } from './checkout-conf/checkout-conf.component';
+import { UserLoginComponent } from './user-login/user-login.component';
+import { MerchantLoginComponent } from './merchant-login/merchant-login.component';
 
 const routes: Routes = [
   {path:'cart', component: CartComponent},
   {path:'checkout', component: CheckoutComponent },
-  {path:'checkout-conf', component: CheckoutConfComponent }
+  {path:'checkout-conf', component: CheckoutConfComponent },
+  {path:'user-login', component: UserLoginComponent },
+  {path:'merchant-login', component: MerchantLoginComponent }
 ];
 
 @NgModule({
--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -7,13 +7,17 @@ import { AppComponent } from './app.component';
 import { CartComponent } from './cart/cart.component';
 import { CheckoutComponent } from './checkout/checkout.component';
 import { CheckoutConfComponent } from './checkout-conf/checkout-conf.component';
+import { UserLoginComponent } from './user-login/user-login.component';
+import { MerchantLoginComponent } from './merchant-login/merchant-login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CartComponent,
     CheckoutComponent,
-    CheckoutConfComponent
+    CheckoutConfComponent,
+    UserLoginComponent,
+    MerchantLoginComponent
   ],
   imports: [
     BrowserModule,
