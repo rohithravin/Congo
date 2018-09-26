80c315fe42f296c4f56b09cefea094aacab31948
Ashwin <mahesh.ashwin1998@gmail.com>
2018-09-26 17:43:57 -0400

    before merge

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ var app=express()
 var path=require('path')
 
 app.use(express.static(path.join(__dirname, './public/dist/public')))
-
+app.all('*', (request, response, next)=>{
+    response.sendFile(path.resolve('./public/dist/public/index.html'))
+})
 app.listen(8000, function(){
     console.log("Server is listening on port 8000")
 })
