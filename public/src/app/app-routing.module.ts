7a134acaba0163166ad5e220db468003a092dbe8
Raman Kahlon <ramankahlon32@gmail.com>
2018-09-28 12:17:55 -0400

    merchant product css

--- public/src/app/app-routing.module.ts
+++ public/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MerchantaddComponent } from './merchantadd/merchantadd.component'
+import { MerchantproductComponent } from './merchantproduct/merchantproduct.component';
 
 const routes: Routes = [
-  {path: 'addProduct', component:MerchantaddComponent}
+  {path: 'addProduct', component:MerchantaddComponent},
+  {path: 'merchantProduct', component:MerchantproductComponent}
 ];
 
 @NgModule({
