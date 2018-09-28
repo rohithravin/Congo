fa6d75d9d7d8596ff51f816dbb01b42934c89b6c
Spencer Kline <kline39@purdue.edu>
2018-09-28 16:05:59 -0400

    search/product routes added

--- public/src/app/app-routing.module.ts
+++ public/src/app/app-routing.module.ts
@@ -7,6 +7,8 @@ import { CheckoutConfComponent } from './checkout-conf/checkout-conf.component';
 import { UserLoginComponent } from './user-login/user-login.component';
 import { MerchantLoginComponent } from './merchant-login/merchant-login.component';
 import { MerchantPortalComponent } from './merchant-portal/merchant-portal.component';
+import { SearchComponent } from './search/search.component';
+import { ProductComponent } from './product/product.component';
 
 const routes: Routes = [
   {path:'', component:HomepageComponent},
@@ -15,7 +17,9 @@ const routes: Routes = [
   {path:'checkout-conf', component: CheckoutConfComponent },
   {path:'user-login', component: UserLoginComponent },
   {path:'merchant-login', component: MerchantLoginComponent },
-  {path:'merchant-portal', component: MerchantPortalComponent }
+  {path:'merchant-portal', component: MerchantPortalComponent },
+  {path: 'search', component: SearchComponent},
+  {path: 'product', component: ProductComponent}
 ];
 
 @NgModule({
