94e56c952cc91e84b861ed6e6591ea0fa725254d
Ashwin <mahesh.ashwin1998@gmail.com>
2018-09-29 18:28:22 -0400

    Database set up complete

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 var express=require('express')
 var app=express()
 var path=require('path')
+var firebase = require("firebase");
+var config = {
+    apiKey: "AIzaSyARn1Y5ytOrRpFjcVKtmR5yQnllMF0gZbg",
+    authDomain: "congo3070.firebaseapp.com",
+    databaseURL: "https://congo3070.firebaseio.com",
+    storageBucket: "congo3070.appspot.com",
+  };
+firebase.initializeApp(config);
+var defaultDatabase = firebase.database();
+
 
 app.use(express.static(path.join(__dirname, './public/dist/public')))
 app.all('*', (request, response, next)=>{
