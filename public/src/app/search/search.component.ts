fbff94fd442874b88c90e1ba3f9163ffae28e0bb
Spencer Kline <kline39@purdue.edu>
2018-10-03 14:33:45 -0400

    refactored template to remove filters

--- public/src/app/search/search.component.ts
+++ public/src/app/search/search.component.ts
@@ -24,13 +24,6 @@ star:string;
 five_star_filter:boolean;
 three_four_star_filter:boolean;
 one_two_star_filter:boolean;
-book_filter:boolean;
-furniture_filter:boolean;
-electronic_filter:boolean;
-four_price_filter:boolean;
-three_price_filter:boolean;
-one_price_filter:boolean;
-relevance_filter:boolean;
 popular_filter:boolean;
 low_high_filter:boolean;
 high_low_filter:boolean;
@@ -47,18 +40,11 @@ categories:string;
       
       
     this.productName = ''
-    this.filterSize = 13;
+    this.filterSize = 6;
     this.star = "checked";
     this.five_star_filter = false;
     this.three_four_star_filter= false;
     this.one_two_star_filter = false;
-    this.book_filter= false;
-    this.furniture_filter = false;
-    this.electronic_filter= false;
-    this.four_price_filter = false;
-    this.three_price_filter = false;
-    this.one_price_filter = false;
-    this.relevance_filter = false;
     this.popular_filter = false;
     this.low_high_filter = false;
     this.high_low_filter = false;
@@ -85,13 +71,6 @@ categories:string;
     this.five_star_filter = false;
     this.three_four_star_filter= false;
     this.one_two_star_filter = false;
-    this.book_filter= false;
-    this.furniture_filter = false;
-    this.electronic_filter= false;
-    this.four_price_filter = false;
-    this.three_price_filter = false;
-    this.one_price_filter = false;
-    this.relevance_filter = false;
     this.popular_filter = false;
     this.low_high_filter = false;
     this.high_low_filter = false;
@@ -121,32 +100,8 @@ categories:string;
     filters = this.getAllSearchFilters();
     console.log(filters);
     console.log("test");
-    let filterFlag: Array<any> = [];
-    let index = 0;
-    this.categories = '';
-    while(index < this.filterSize){
-      if (filters[index]){
-        filterFlag.push(1);
-        switch(index){
-          case(3):
-          this.categories += " Books ";
-          break;
-          case(4):
-          this.categories += " Furniture ";
-          break;
-          case(5):
-          this.categories += " Electronics ";
-          break;
-          default:
-          break;
-        }
-      }else{
-        filterFlag.push(0);
-      }
-
-      index++;
-    }
-    console.log(filterFlag);
+    
+    
   }
 
   setThreeFourStarFilter(){
@@ -165,62 +120,7 @@ categories:string;
     }
   }
 
-  setBookFilter() {
-    if (!this.book_filter){
-      this.book_filter = true;
-    }else {
-      this.book_filter= false;
-    }
-  }
-
-  setFurnitureFilter () {
-    if (!this.furniture_filter){
-      this.furniture_filter = true;
-    }else {
-      this.furniture_filter= false;
-    }
-  }
-
-  setElectronicFilter() {
-    if (!this.electronic_filter){
-      this.electronic_filter = true;
-    }else {
-      this.electronic_filter = false;
-    }
-  }
-
-  setFourPriceFilter () {
-    if (!this.four_price_filter){
-      this.four_price_filter = true;
-    }else {
-      this.four_price_filter = false;
-    }
-  }
-
-  setThreePriceFilter() {
-    if (!this.three_price_filter){
-      this.three_price_filter = true;
-    }else {
-      this.three_price_filter = false;
-    }
-  }
-
-  setOnePriceFilter() {
-    if (!this.one_price_filter){
-      this.one_price_filter = true;
-    }else {
-      this.one_price_filter = false;
-    }
-  }
-
-  setRelevanceFilter() {
-    if (!this.relevance_filter){
-      this.relevance_filter = true;
-    }else {
-      this.relevance_filter = false;
-    }
-  }
-
+  
   setPopularFilter() {
     if (!this.popular_filter){
       this.popular_filter = true;
@@ -252,13 +152,6 @@ categories:string;
     filters.push(this.five_star_filter);
     filters.push(this.three_four_star_filter);
     filters.push(this.one_two_star_filter);
-    filters.push(this.book_filter);
-    filters.push(this.furniture_filter);
-    filters.push(this.electronic_filter);
-    filters.push(this.four_price_filter);
-    filters.push(this.three_price_filter);
-    filters.push(this.one_price_filter);
-    filters.push(this.relevance_filter);
     filters.push(this.popular_filter);
     filters.push(this.low_high_filter);
     filters.push(this.high_low_filter);
