dbb71993cda66d66ff317e80054a06597051177e
Ashwin <mahesh.ashwin1998@gmail.com>
2018-10-04 18:50:07 -0400

    Mongo database created

--- server.js
+++ server.js
@@ -31,19 +31,70 @@ var User = mongoose.model('User')
 
 var MerchantSchema = new mongoose.Schema({
     name:{type:String, required:[true, "Merchant name is required"], minlength:3},
-    user_id={type:String, required:true},
+    user:UserSchema,
     description:{type:String, required:[true, "Description is required"]},
-    products:[ProductSchema],
-})
+    products:["ProductSchema"],
+}, {timestamps:true})
 mongoose.model('Merchant', MerchantSchema)
 var Merchant = mongoose.model('Merchant');
 
 var ProductSchema=new mongoose.Schema({
-    
+    name:{type:String, required:[true, "Product name is required"], minlength:5},
+    price:{type:Number, required:[true, "Price is required"], min:0},
+    description:{type:String, required:[true, "Description is required"], minlength:10},
+    sizes:[{type:String}],
+    colors:[{type:String}],
+    num_sold:{type:Number, default:0},
+    num_views:{type:Number, default:0},
+    images:[{type:String}],
+    tags:[{type:String}],
+    merchant:MerchantSchema,
+    reviews:["ReviewSchema"]
+}, {timestamps:true})
+mongoose.model('Product', ProductSchema)
+var Product=mongoose.model('Product')
+
+var BigBannerSchema=new mongoose.Schema({
+    image:{type:String, required:[true, "Image is required"]},
+    active:{type:Boolean, default:true},
+    product:ProductSchema
+}, {timestamps:true})
+mongoose.model('BigBanner', BigBannerSchema)
+var BigBanner=mongoose.model('BigBanner')
+
+var ReviewSchema= new mongoose.Schema({
+    user:UserSchema,
+    product:ProductSchema,
+    rating:{type:Number, required:true, min:1, max:5},
+    review:{type:String, required:[true, "A review is required"], minlength:20}
 })
+mongoose.model('Review', ReviewSchema)
+var Review=mongoose.model('Review')
 
+var CartSchema = new mongoose.Schema({
+    user:UserSchema,
+    items:[ProductSchema],
+    total:{type:Number, default:0}
+})
+mongoose.model('Cart', CartSchema)
+var Cart=mongoose.model('Cart')
 
-
+var OrderSchema=new mongoose.Schema({
+    total:{type:Number, default:0},
+    shipping:{type:Number, default:0},
+    user:UserSchema,
+    stripe_key:{type:String, required:true},
+    //Change ProductSchema to orderItemSchema and in cart schema, create cart item
+    items:[ProductSchema],
+    refunded:{type:Boolean, default:false},
+    street_address:{type:String, required:[true, "Street address is required"]},
+    city:{type:String, required:[true, "Street address is required"]},
+    state:{type:String, required:[true, "State is required"]},
+    zip_code:{type:String, required:[true, "Zip Code is required"]},
+    country:{type:String, default:'United States'}
+})
+mongoose.model('Order', OrderSchema)
+var Order=mongoose.model('Order')
 
 app.use(express.static(path.join(__dirname, './public/dist/public')))
 
