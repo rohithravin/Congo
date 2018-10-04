b24459111438e05724010350cc2b665cf872e6ec
Ashwin <mahesh.ashwin1998@gmail.com>
2018-10-04 17:28:27 -0400

    Setting up mongoDN

--- server.js
+++ server.js
@@ -1,16 +1,9 @@
 var express=require('express')
 var app=express()
 var path=require('path')
-// var firebase = require("firebase");
-// var config = {
-//     apiKey: "AIzaSyARn1Y5ytOrRpFjcVKtmR5yQnllMF0gZbg",
-//     authDomain: "congo3070.firebaseapp.com",
-//     databaseURL: "https://congo3070.firebaseio.com",
-//     storageBucket: "congo3070.appspot.com",
-// };
-// firebase.initializeApp(config);
-// var db = firebase.database();
+var mongoose=require('mongoose')
 
+// Firestore DB
 var admin = require("firebase-admin");
 var serviceAccount = require("./serviceAccountKey.json");
 admin.initializeApp({
@@ -20,6 +13,37 @@ admin.initializeApp({
 var db = admin.firestore();
 const settings = {timestampsInSnapshots: true};
 db.settings(settings);
+
+// Mongoose DB
+mongoose.connect('mongodb://localhost/CongoDB')
+var UserSchema = new mongoose.Schema({
+    first_name:{type:String, required:[true, "First name is required."], minlength:2},
+    last_name:{type:String, required:[true, "Last name is required"], minlength:2},
+    email:{type:String, required: [true, "Email is required"], minlength:5},
+    password:{type:String, required:[true, "Password is required"], minlength:[8, "Password must be atleast 8 characters"]},
+    user_level:{type:Number, default:1},
+    phone_number:{type:String, required:[true, "Phone number is required"], minlength:[10, "Invalid phone number"], maxlength:[10, "Invalid phone number"]},
+    stream:{type:Boolean, default:false, required:[true, "Stream is required"]},
+    cart:["CartSchema"]
+}, {timestamps:true});
+mongoose.model('User', UserSchema)
+var User = mongoose.model('User')
+
+var MerchantSchema = new mongoose.Schema({
+    name:{type:String, required:[true, "Merchant name is required"], minlength:3},
+    user_id={type:String, required:true},
+    description:{type:String, required:[true, "Description is required"]},
+    products:[ProductSchema],
+})
+mongoose.model('Merchant', MerchantSchema)
+var Merchant = mongoose.model('Merchant');
+
+var ProductSchema=new mongoose.Schema({
+    
+})
+
+
+
 
 app.use(express.static(path.join(__dirname, './public/dist/public')))
 
