e6b2956b2efabefb0803281f9a9216b51c84031f
Spencer Kline <kline39@purdue.edu>
2018-10-04 23:49:46 -0400

    search page

--- server.js
+++ server.js
@@ -219,11 +219,12 @@ app.post('/createDummyProduct', function(request, response){
     // if(!('loggedIn' in request.session) || ('loggedIn' in request.session && request.session.loggedIn==false)){
     //     return response.json({success:0, message:'User not logged in'})
     // }
-    console.log("Doing this for the change")
+    // console.log("Doing this for the change")
     var product=request.body['product']
     console.log(product)
-    var newProduct=new Product({name:product.name, price:product.price, description:product.description, sizes:[product.size], colors:[product.color], images:[product.image], tags:[product.tag]})
+    var newProduct=new Product({name:product.name, price:parseFloat(product.price), description:product.description, sizes:[product.size], colors:[product.color], images:[product.image], tags:[product.tag]})
     newProduct.save(function(error){
+        console.log("Inside save function")
         if(error){
             response.json({success:0, message:"There was an error creating your product"})
         }
