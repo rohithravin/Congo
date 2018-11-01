9c57fe623c4db3ba28ace1286932453b19cdf489
Spencer Kline <kline39@purdue.edu>
2018-11-01 19:43:53 -0400

    Purchase History

--- public/src/app/purchase-history/purchase-history.component.ts
+++ public/src/app/purchase-history/purchase-history.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpService} from '../http.service';
+import {Router} from '@angular/router';
+
 
 @Component({
   selector: 'app-purchase-history',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./purchase-history.component.css']
 })
 export class PurchaseHistoryComponent implements OnInit {
+  userID:any;
+  orders:any;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private _httpService:HttpService,private _router:Router) { 
+    this.userID = localStorage.getItem('userID');
+    this.orders = [];
   }
 
+  ngOnInit() {
+    this.getUserOrders();
+  }
+
+
+  getUserOrders(){
+    if(localStorage.getItem('loggedIn') == 'false'){
+      this._router.navigate(['login']);
+    }
+    var orderObs=this._httpService.getOrders(this.userID);
+    orderObs.subscribe(data=>{
+      if(data['success'] != 1){
+        console.log("error");
+      }else{
+      this.orders = data['orders'];
+      console.log(this.orders);
+      }
+    })
+  }
 }
