f7094796a3845f0d50a578033288dfee45869b42
Spencer Kline <kline39@purdue.edu>
2018-10-31 20:02:35 -0400

    Revert "Featured Products Empty bug"
    This reverts commit 5aa0c49237effebdeac981794dc8b973235285f6.

--- public/src/app/homepage/homepage.component.ts
+++ public/src/app/homepage/homepage.component.ts
@@ -10,16 +10,10 @@ export class HomepageComponent implements OnInit {
   bigBannerPromo:any;
   smallBannerPromo:any;
   featuredProductsPromo:any;
-  showBB:boolean;
-  showSB:boolean;
-  showFP:boolean;
   constructor(private _httpService:HttpService) {
     this.bigBannerPromo = [];
     this.smallBannerPromo = [];
     this.featuredProductsPromo = [];
-    this.showBB = false;
-    this.showSB = false;
-    this.showFP = false;
    }
 
   ngOnInit() {
@@ -30,28 +24,9 @@ export class HomepageComponent implements OnInit {
     var featuredObs=this._httpService.fetchFeatured()
     featuredObs.subscribe(data=>{
       console.log(data)
-      
-      if(data['success'] == 1){
-        if(data['bigBanner'].length > 0){
-          this.bigBannerPromo = data['bigBanner'];
-          this.showBB = true;
-        }else if(data['bigBanner'].length == 0){
-          this.showBB = false;
-        }
-
-        if(data['smallBanner'].length > 0){
-          this.smallBannerPromo = data['smallBanner'];
-          this.showSB = true;
-        }else if(data['smallBanner'].length == 0){
-          this.showSB = false;
-        }
-        if(data['featuredProducts'].length > 0){
-          this.featuredProductsPromo = data['featuredProducts'];
-          this.showFP = true;
-        }else if(data['featuredProducts'].length == 0){
-          this.showFP = false;
-        }
-      }
+      this.bigBannerPromo = data['bigBanner'];
+      this.smallBannerPromo = data['smallBanner'];
+      this.featuredProductsPromo = data['featuredProducts'];
       console.log(this.smallBannerPromo);
       console.log(this.featuredProductsPromo);
     }) 
