74bd6aa3d4dcfa9c051edc57f0f202de22e23832
Rohith Ravindranath <rohithravin@gmail.com>
2018-11-14 20:29:53 -0500

    merchant orders ui complete

--- public/src/app/app-routing.module.ts
+++ public/src/app/app-routing.module.ts
@@ -24,6 +24,7 @@ import { AdminPortalComponent } from './admin-portal/admin-portal.component';
 import { StreamRegistrationComponent } from './stream-registration/stream-registration.component';
 import { AdminMerchantsComponent} from './admin-merchants/admin-merchants.component';
 import { UserOrderHistoryComponent } from './user-order-history/user-order-history.component';
+import { MerchantOrdersComponent } from './merchant-orders/merchant-orders.component';
 
 import { GiftCardComponent } from './gift-card/gift-card.component';
 const routes: Routes = [
@@ -50,7 +51,8 @@ const routes: Routes = [
   {path: 'admin-portal', component: AdminPortalComponent},
   {path: 'stream/register', component:StreamRegistrationComponent},
   {path: 'admin/merchants', component: AdminMerchantsComponent},
-  {path: 'user-order-history', component: UserOrderHistoryComponent}
+  {path: 'user-order-history', component: UserOrderHistoryComponent},
+  {path: 'merchant-orders', component: MerchantOrdersComponent}
    //Path any, = home
 ];
 
