3c668ea411117dc912258e124937974bad3bb8fe
Rohith Ravindranath <rohithravin@gmail.com>
2018-11-14 20:01:55 -0500

    user-order-history ui

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -30,6 +30,7 @@ import { AdminPortalComponent } from './admin-portal/admin-portal.component';
 import { StreamRegistrationComponent } from './stream-registration/stream-registration.component';
 import { AdminMerchantsComponent } from './admin-merchants/admin-merchants.component';
 import { GiftCardComponent } from './gift-card/gift-card.component';
+import { UserOrderHistoryComponent } from './user-order-history/user-order-history.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -56,7 +57,8 @@ import { GiftCardComponent } from './gift-card/gift-card.component';
     AdminPortalComponent,
     StreamRegistrationComponent,
     AdminMerchantsComponent,
-    GiftCardComponent
+    GiftCardComponent,
+    UserOrderHistoryComponent
   ],
   imports: [
     BrowserModule,
