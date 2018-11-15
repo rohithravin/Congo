e6baf40646b031acaf94a906e89df4ddb9de4df9
Kenny Varghese <varghese.kenny@gmail.com>
2018-11-14 19:13:15 -0500

    Finished giftcard UI

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -28,8 +28,8 @@ import { PurchaseHistoryComponent } from './purchase-history/purchase-history.co
 import { AdminLoginComponent } from './admin-login/admin-login.component';
 import { AdminPortalComponent } from './admin-portal/admin-portal.component';
 import { StreamRegistrationComponent } from './stream-registration/stream-registration.component';
-import { AdminMerchantsComponent } from './admin-merchants/admin-merchants.component'
-import { GiftCardComponent } from './gift-card/gift-card.component'
+import { AdminMerchantsComponent } from './admin-merchants/admin-merchants.component';
+import { GiftCardComponent } from './gift-card/gift-card.component';
 @NgModule({
   declarations: [
     AppComponent,
